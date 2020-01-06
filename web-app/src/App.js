2a263852f68ebcbde404af960edef35a9bcf3d08
Fang Nan <nanfang05@gmail.com>
2020-01-05 19:02:46 -0800

    Load and show profile

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./utils/react-auth0-spa";
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
   const { loading } = useAuth0();
@@ -8,12 +12,18 @@ function App() {
   if (loading) {
     return <div>Loading...</div>;
   }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- web-app/src/components/NavBar.js
+++ web-app/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "../utils/react-auth0-spa";
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -11,6 +13,14 @@ const NavBar = () => {
             )}
 
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+            {/* NEW - add a link to the home and profile pages */}
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>&nbsp;
+                    <Link to="/profile">Profile</Link>
+                </span>
+            )}
         </div>
     );
 };
